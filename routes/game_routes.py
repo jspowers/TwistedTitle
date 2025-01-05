3eb27787c7f5b3fc3835850ec5f86b559d5fdbd4
James Powers <47932894+jspowers@users.noreply.github.com>
2025-01-05 14:03:10 -0600

    Build attempt history

--- routes/game_routes.py
+++ routes/game_routes.py
@@ -12,9 +12,13 @@ def index():
 
     clue = title_prompts[1]
 
-    if "attempts" not in session:
-        session["attempts"] = 0
+    print(session["attempt_list"])
+
+    if "attempt" not in session:
+        session["attempt_list"] = {i:{"answer":None,"correct":None} for i in range(1,MAX_ATTEMPTS+1)}
+        session["attempt"] = 0
         session["game_over"] = False
+        
 
     # check the user answer
     if request.method == "POST":
@@ -25,21 +29,27 @@ def index():
             return redirect(url_for("game.index"))
 
         # if session has hit max attempts to answer
-        user_answer = request.form.get("answer", "").strip().lower()
-        session["attempts"] += 1
-
+        user_answer = request.form.get("answer", "").strip().upper()
+        session["attempt"] += 1
+        attempt = str(session["attempt"])
+        session["attempt_list"][attempt]['answer'] = user_answer
+        
         # if the user answer is correct
-        if user_answer == clue.twisted_title:
-            session["message"] = "Correct! You solved the riddle!"
-            session['correct_response'] = session['attempts']
+        if user_answer == clue.twisted_title.upper():
+            session["attempt_list"][attempt]['correct'] = True
             session["game_over"] = True
         
+
         # if the page loads and the max attempts are reached
-        elif session["attempts"] >= MAX_ATTEMPTS:
-            session["message"] = f"Game Over! The correct answer was '{clue.twisted_title}'."
+        elif session["attempt"] >= MAX_ATTEMPTS:    
+            session["attempt_list"][attempt]['correct'] = False
+            session["message"] = f"Game Over! The correct answer was '{clue.twisted_title.upper()}'."
             session["game_over"] = True
+        
         else:
-            session["message"] = f"Incorrect! You have {MAX_ATTEMPTS - session['attempts']} attempts left."
+            session["attempt_list"][attempt]['correct'] = False
+        # else:
+        #     session["message"] = f"Incorrect! You have {MAX_ATTEMPTS - session['attempt']} attempts left."
 
         return redirect(url_for("game.index"))
     
@@ -48,15 +58,16 @@ def index():
         current_user=current_user,
         clue=clue.prompt, 
         message=session.get("message", ""),
-        attempts=session.get("attempts", ""),
+        attempt_list=session.get("attempt_list", dict()),
         correct_attempt=session.get("correct_response", None),
         )
 
 @game_blueprint.route("/reset")
 def reset():
-    session["attempts"] = 0
+    session.pop("attempt", None)
     session["game_over"] = False
     session['correct_response'] = None
+    session.pop("message", None)
     return redirect(url_for("game.index"))
 
 @game_blueprint.route("/about")
